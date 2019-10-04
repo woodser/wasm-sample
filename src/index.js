e758b97187bdace79372f60aa8fda6bbdb834e6a
woodser <woodser@protonmail.com>
2019-10-04 11:42:35 -0400

    cleanup header comment

--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
 /**
- * TODO
+ * Starts the web application and demonstrates using two versions of Foo.
+ * One version is implemented in native JavaScript.
+ * Another version is implemented by wrapping C++ using WebAssembly.
  */
-
 "use strict"
 
 // imports
 const assert = require("assert");
 const FooLocal = require("./js/FooLocal");
 const FooWasm = require("./js/FooWasm");
-
-//import FooWasm from "./js/WasmSample_WASM.js";
-//import FooWasmModule from "./js/WasmSample_WASM.wasm";
-//
-////Since webpack will change the name and potentially the path of the 
-////`.wasm` file, we have to provide a `locateFile()` hook to redirect
-////to the appropriate URL.
-////More details: https://kripken.github.io/emscripten-site/docs/api_reference/module.html
-//const module = FooWasm({
-//  locateFile(path) {
-//   if (path.endsWith('.wasm')) {
-//     return FooWasmModule;
-//   }
-//   return path;
-//  }
-//});
-//
-//module.onRuntimeInitialized = () => {
-//  console.log("Ready?");
-//  console.log(module);
-//};
 
 // start the application
 startApp();
@@ -46,6 +26,3 @@ async function startApp() {
     console.log("Foo wasm says: " + fooWasm.getMessage());
   });
 }
-
-
-
